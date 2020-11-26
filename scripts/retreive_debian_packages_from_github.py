68e19badc112274f7b26d99b8ff30463c4eb8ff3
Benoit Brisset <benoit.brisset@gmail.com>
2020-11-26 18:00:04 +0100

    deploy navitia on artemis with jenkinsfile

--- scripts/retreive_debian_packages_from_github.py
+++ scripts/retreive_debian_packages_from_github.py
@@ -43,7 +43,9 @@ import requests
 import wget
 import sys
 import os
+import glob
 import argparse
+import zipfile
 
 DEFAULT_WORKFLOW_NAME = "Build Navitia Packages For Dev"
 DEFAULT_OUTPUT_PATH = "."
@@ -118,11 +120,11 @@ class GithubArtifactsReceiver:
         self.logger.info("download {}".format(zip_url.split("@")[1]))
         filename = wget.download(zip_url, self.artifacts_output_path + "/" + self.artifacts_name)
         self.logger.info("File {} downloaded".format(filename))
-        
+
         expected_file_size = artifact_info["size_in_bytes"]
         file_size = os.path.getsize(filename)
         delta_size = abs(expected_file_size - file_size)
-        percent_diff_size = delta_size * 100. / file_size 
+        percent_diff_size = delta_size * 100. / file_size
         assert percent_diff_size < 0.1, "Downloaded file size ({} bytes) differs from expected size ({} bytes). \nDownload may have failed or disk may be full".format(file_size, expected_file_size)
 
 
@@ -158,6 +160,25 @@ class GithubArtifactsReceiver:
         self.download_artifacts(artifacts_url, run_id)
 
 
+def uncompress_artifacts(path_to_artifacts):
+    """ uncompress the downloaded artifacts """
+    zip = zipfile.ZipFile(path_to_artifacts)
+    zip.extractall()
+    zip = zipfile.ZipFile('navitia_debian_packages.zip')
+    zip.extractall()
+    zip.close()
+
+
+def remove_all_artifacts(path_to_artifacts):
+    """ remove old artifacts """
+    for f in glob.glob("navitia-*"):
+        os.remove(f)
+    if os.path.isfile("navitia_debian_packages.zip"):
+        os.remove("navitia_debian_packages.zip")
+    if os.path.isfile(path_to_artifacts):
+        os.remove(path_to_artifacts)
+
+
 def config_logger():
     logger = logging.getLogger('retreive debian package from github')
     logger.setLevel(logging.DEBUG)
@@ -175,13 +196,15 @@ def parse_args(parser, logger):
     parser.add_argument("-w", dest="workflow_name", help="Github Workflow name", default=DEFAULT_WORKFLOW_NAME)
     parser.add_argument("-a", dest="artifacts_name", help="Artifacts name", default=DEFAULT_ARTIFACTS_NAME)
     parser.add_argument("-o", dest="output_dir", help="Output path", default=DEFAULT_OUTPUT_PATH)
+    parser.add_argument("-r", dest="remove_artifacts", help="Remove artifacts in current directory")
+    parser.add_argument("-z", dest="uncompress_artifacts", help="Uncompress the downloaded artifacts")
     args = parser.parse_args()
 
-    if not args.github_user:
+    if not args.github_user and not args.remove_artifacts and not args.uncompress_artifacts:
         logger.error("Github user is a mandatory parameter")
         logger.error("Please fill -u parameter")
         sys.exit("Github user is a mandatory parameter")
-    if not args.github_user:
+    if not args.github_user and not args.remove_artifacts and not args.uncompress_artifacts:
         logger.error("Github token is a mandatory parameter")
         logger.error("Please fill -t parameter")
         sys.exit("Github token is a mandatory parameter")
@@ -195,9 +218,17 @@ def main():
 
     args = parse_args(argparse.ArgumentParser(), logger)
 
-    artifacts_receiver = GithubArtifactsReceiver(args.github_user, args.github_token, args.workflow_name, args.artifacts_name, args.output_dir)
-    artifacts_receiver.check_github_api()
-    artifacts_receiver.run()
+    if args.remove_artifacts:
+        logger.info("remove old artifacts")
+        remove_all_artifacts(DEFAULT_ARTIFACTS_NAME)
+    elif args.uncompress_artifacts:
+        logger.info("uncompress the downloaded artifacts")
+        uncompress_artifacts(DEFAULT_ARTIFACTS_NAME)
+        logger.info("you can have now acces to navitia-*.deb packages")
+    else:
+        artifacts_receiver = GithubArtifactsReceiver(args.github_user, args.github_token, args.workflow_name, args.artifacts_name, args.output_dir)
+        artifacts_receiver.check_github_api()
+        artifacts_receiver.run()
 
     logger.info("finish process")
 
